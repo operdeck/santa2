f07418e52183a83fe4113664cafd2c8c94b1687e
Otto Perdeck <perdo@pega.com>
2016-12-02 17:33:02 +0100

    Graphs and reporting fixed. Different XGB metric. Local run at 500 rounds says 0.1369426. Kaggle says .

--- buildProductModels.R
+++ buildProductModels.R
@@ -91,12 +91,18 @@ train <- train[action == 1,] # only keep the additions
 train$action <- NULL
 cat("Train size after melting:", dim(train), "; unique persons:",length(unique(train$ncodpers)),fill = T)
 
-ggplot(group_by(train, product, dataset) %>% 
-         summarise(additions = n()/length(unique(train$ncodpers))) %>%
-         mutate(additions = ifelse(dataset == "Validate", additions/validationRatio, additions/(1-validationRatio))), 
-       aes(product,additions,fill=dataset))+
+productDistributions <-
+  group_by(train, product, dataset) %>%
+  summarise(additions = n()) %>%
+  left_join(group_by(train, dataset) %>% summarise(dataset.total = n()), by="dataset") %>%
+  mutate(additions.rel = additions/dataset.total) %>% 
+  arrange(additions.rel)
+
+print(ggplot(productDistributions, 
+       aes(factor(product, levels=unique(productDistributions$product)),additions.rel,fill=dataset))+
   geom_bar(stat="identity",position="dodge")+coord_flip()+
-  scale_y_continuous(labels = percent)+ggtitle("Additions per customer by product")
+  scale_y_continuous(labels = percent)+ggtitle("Additions by product")+
+    xlab("product")+ylab("Added"))
 
 cat("Unique customers in train set that made a purchase:",length(unique(train[dataset=="Train",ncodpers])),fill=T)
 
@@ -109,10 +115,14 @@ train <- merge(train, aggregates, all.x=TRUE)
 test <- merge(test, aggregates, all.x=TRUE)
 
 # Build multiclass model
-xgb.params <- list("objective" = "multi:softprob",
+xgb.params <- list(objective = "multi:softprob",
+                   eval_metric = "mlogloss", # i really want "map@7" but get errors
                    max.depth = 5,
                    num_class = length(productFlds),
                    eta = 0.01)
+
+# See https://github.com/dmlc/xgboost/blob/master/R-package/demo/custom_objective.R 
+# for custom error function
 
 modelTrainFlds <- names(train)[which(!names(train) %in% c("product","dataset",productFlds))]
 trainMatrix <- xgb.DMatrix(data.matrix(train[dataset == "Train", modelTrainFlds, with=F]), 
@@ -124,58 +134,82 @@ validateMatrix <- xgb.DMatrix(data.matrix(train[dataset == "Validate", modelTrai
 
 bst = xgb.train(params=xgb.params, data = trainMatrix, missing=NaN,
                 watchlist=list(train=trainMatrix, validate=validateMatrix),
-                nrounds=100, 
+                nrounds=500, 
                 maximize=F)
 
 # Compute & plot feature importance matrix & summary tree
+print("Feature importance matrix...")
 importance_matrix <- xgb.importance(modelTrainFlds, model = bst)
 print(xgb.plot.importance(importance_matrix))
 
 # xgb.plot.tree(feature_names = dimnames(trainMatrix)[[2]], model = bst, n_first_tree = 2)
 # xgb.plot.multi.trees(model = bst, feature_names = dimnames(trainMatrix)[[2]], features.keep = 3)
 
+print("Test/Validate predictions...")
+xgbpred <- predict(bst, validateMatrix, missing=NaN)
+validateProbabilities <- t(matrix(xgbpred, nrow=length(productFlds), ncol=sum(train$dataset == "Validate")))
+colnames(validateProbabilities) <- productFlds
+
 testMatrix <- xgb.DMatrix(data.matrix(test[, modelTrainFlds, with=F]), missing=NaN)
 xgbpred <- predict(bst, testMatrix, missing=NaN)
-probs <- t(matrix(xgbpred, nrow=length(productFlds), ncol=nrow(test)))
-colnames(probs) <- productFlds
+testProbabilities <- t(matrix(xgbpred, nrow=length(productFlds), ncol=nrow(test)))
+colnames(testProbabilities) <- productFlds
 
-# not completely correct - as we do 1 entry
-trnSummary <- 
-  spread(mutate(select(train, dataset, product, ncodpers), value=1), product, value, fill=0)
+# Viz distributions together with the earlier ones from the actual data
+print("Test/Validate distributions")
+testDistrib <- data.frame(
+  product   = productFlds,
+  additions = rowSums(apply(-testProbabilities, 1, rank, ties.method = "first") <= 7),
+  dataset   = "Test Predictions")
+testDistrib$dataset.total <- sum(testDistrib$additions) 
+testDistrib$additions.rel <- testDistrib$additions/testDistrib$dataset.total 
 
-rsltsData <- data.frame(product=colnames(probs), 
-                        model=colMeans(probs), 
-                        train=(colSums(dplyr::select(trnSummary, -dataset, -ncodpers), na.rm=T) / length(unique(train$ncodpers))) [colnames(probs)],
-                        stringsAsFactors = F) %>% gather(src, meanprob, -product)
-print(ggplot(rsltsData, aes(product, meanprob, fill=src))+
-  geom_bar(stat="identity", position="dodge")+coord_flip())
+validateDistrib <- data.frame(
+  product   = productFlds,
+  additions = rowSums(apply(-validateProbabilities, 1, rank, ties.method = "first") <= 7),
+  dataset   = "Validate Predictions")
+validateDistrib$dataset.total <- sum(validateDistrib$additions) 
+validateDistrib$additions.rel <- validateDistrib$additions/validateDistrib$dataset.total 
+productDistributions <- rbindlist(list(productDistributions, testDistrib, validateDistrib), use.names = T, fill=T)
 
-# What do I get when just submitting using base propensities?
+print(ggplot(productDistributions, 
+             aes(factor(product, levels=unique(arrange(productDistributions,additions.rel)$product)),additions.rel,fill=dataset))+
+        geom_bar(stat="identity",position="dodge")+coord_flip()+
+        scale_y_continuous(labels = percent)+ggtitle("Additions by product")+
+        xlab("product")+ylab("Added"))
 
 print("Assembling results...")
 testResults <- data.frame(ncodpers = test[, ncodpers])
-testResults$added_products <- apply(probs, 1, 
+testResults$added_products <- apply(testProbabilities, 1, 
                                     function(row) { 
                                       paste(names(sort(rank(-row, ties.method = "first")))[1:7], collapse=" ") })
 print("Writing submission file...")
 write.csv(testResults, "data/newsubmission.csv",row.names = F, quote=F)
-zip("data/newsubmission.csv.zip","data/newsubmission.csv")
 
-stop("Old stuff below")
+# Get validation set back into original 'wide' format
+validationTruth <- spread(mutate(train[dataset == "Validate", 
+                     c("fecha_dato","ncodpers","product"), with=F], value=1),
+       product, value, fill=0)
+for (f in setdiff(productFlds,names(validationTruth))) {
+  validationTruth[[f]] <- 0
+}
+validationTruth <- validationTruth[, c("fecha_dato","ncodpers", productFlds)]
 
-# TODO fix this
+# Get validation predictions but remove the duplicates (these happened because the 
+# data was melted to long format)
+validationWideRowz <- which( !duplicated(data.matrix(train[dataset == "Validate", c("fecha_dato","ncodpers"), with=F])) )
+validationPredicitons <- validateProbabilities[validationWideRowz,productFlds]
 
 # Calculate score on validation set - TODO get this much faster
 avgprecision <- 0
-for (i in 1:nrow(validateMatrix)) {
-  if (i %% 1000 == 0) { 
-    print(i) 
-    print(avgprecision/i)
-  }
-  truth <- all[validateRowz[i], outcomeCols, with=F]
-  predranks <- rank(-validateResults[i, outcomeCols], ties.method = "first")
-  avgprecision <- avgprecision + mapk(truth == "Added", predranks, 7)
+for (i in 1:nrow(validationTruth)) {
+  truth <- validationTruth[i,productFlds]
+  predranks <- rank(-validationPredicitons[i, productFlds], ties.method = "first")
+  avgprecision <- avgprecision + mapk(truth == 1, predranks, 7)
 }
-avgprecision <- avgprecision/nrow(validateMatrix)
+avgprecision <- avgprecision/nrow(validationTruth)
 cat("Average mean precision on validation set:",avgprecision,fill=T)
+
+
+zip("data/newsubmission.csv.zip","data/newsubmission.csv")
 
