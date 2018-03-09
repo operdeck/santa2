c9f23f8f4e567df727849e874422374b708ec203
Otto Perdeck <perdo@pega.com>
2018-03-09 17:20:37 +0100

    Dumps some more info for analysis with Pega

--- dump_data_for_Pega.R
+++ dump_data_for_Pega.R
@@ -6,12 +6,20 @@
 #
 # - santa_snapshots : just the list of customer ID and and snapshot month 
 # - santa_portfolio : current portfolio of customer at snapshot month
-# - santa_additions : the product additions (used to drive the outcomes of the models)
+# - santa_additions : the product additions/removals (used to drive the outcomes of the models)
 # - santa_profile   : the customer profile at that moment
+# - santa_interactionsIHStyle : just the customer additions/removals in "tall" format: customer ID / time / product / action suitable for IH insertion
 #
 # These need to be uploaded to PRPC in the corresponding file datasets. Then, in PRPC,
-# they should be "converted" to DDS datasets (copied) so they can be used as secondary
-# source and support batch partitions.
+# "converted" to DDS datasets (copied) so they can be used as secondary source and support 
+# partitioning. The "Prepare Data" data flow run will do all of this. Monitor the batch 
+# processing landing page to verify that all is completed.
+#
+# Then
+# - FillIH (data flow in InteractionsIHStyle) will take the tall format interactions and put them in IH (for months 5..16, so covering 1 year). These
+#   IH facts will be used by IH summaries and/or IH imports
+# - FillESM (data flow in SantaProducts) will take the broad format additions, runs months 5..16 through ESM and creates 
+#   one or more datasets keyed by customerID with aggregates
 #
 # After these prep steps, the main data flow that composes them and trains and/or runs models
 # can be kicked off. The main flow lives in "Data-Customer-SantaSnapshot" and composes the
@@ -28,6 +36,9 @@
 
 library(plyr)
 library(tidyverse)
+library(data.table)
+
+set.seed(1966)
 
 data_folder <- "data"
 # data_folder <- "data-unittest"
@@ -64,7 +75,7 @@ sampleCustomers <- sample(test$CustomerID, 0.10*length(unique(train$CustomerID))
 train <- train[CustomerID %in% sampleCustomers]
 test <- test[CustomerID %in% sampleCustomers]
 
-cat("Train sampled size:", dim(train), "; unique persons:",length(unique(train$CustomerID)),fill = T)
+cat("Train resampled size:", dim(train), "; unique persons:",length(unique(train$CustomerID)),fill = T)
 
 print("Convert dates to numbers")
 for(f in intersect(data_dateFlds, names(train))) { 
@@ -92,7 +103,7 @@ additions$CustomerID <- joinedwithprev$CustomerID
 additions$Snapshot <- joinedwithprev$Snapshot
 
 print("Expected base propensities")
-print(sapply(outcomefields, function(x) {return(sprintf("%.4f %%",100*length(which(additions[[x]]==1))/nrow(additions)))}))
+print(sort(sapply(outcomefields, function(x) {return(sprintf("%.4f %%",100*length(which(additions[[x]]==1))/nrow(additions)))})))
 
 print("Extract profile")
 
@@ -106,11 +117,83 @@ profile[is.na(profile)] <- -9999
 
 print("Dump files")
 
+setorder(portfolio, Snapshot, CustomerID)
+setorder(additions, Snapshot, CustomerID)
+setorder(profile, Snapshot, CustomerID)
+
+cat("Snapshots:", nrow(portfolio), "snapshots", "for", length(unique(portfolio$CustomerID)), "unique customers", fill=T)
+cat("Additions:", nrow(additions), "additions", "for", length(unique(additions$CustomerID)), "unique customers", fill=T)
+cat("Profile:", uniqueN(select(profile, -Snapshot, -fecha_alta)), "unique profiles", "for", length(unique(profile$CustomerID)), "unique customers", fill=T)
+
 write.csv(portfolio[, c("CustomerID", "Snapshot")], paste(data_folder,"santa_snapshots.csv",sep="/"), row.names=F)
 write.csv(portfolio, paste(data_folder,"santa_currentportfolio.csv",sep="/"), row.names=F)
 write.csv(additions, paste(data_folder,"santa_additions.csv",sep="/"), row.names=F)
 write.csv(profile, paste(data_folder,"santa_profile.csv",sep="/"), row.names=F)
 
+# IH-style version of the additions with customer ID, date, proposition and outcome
+interactionsIHStyle <- as.data.table(additions %>% gather(key="Proposition", value="Outcome", -CustomerID, -Snapshot) %>% filter(Outcome != 0))
+setorder(interactionsIHStyle, Snapshot, CustomerID)
+cat("IH interactions:",nrow(interactionsIHStyle),"additions only:",nrow(interactionsIHStyle[Outcome==1]),fill=T)
+write.csv(interactionsIHStyle, paste(data_folder,"santa_interactionsIHStyle.csv",sep="/"), row.names=F)
+
+print("Mid size set")
+
+midsizeCustomers <- sort(sample(portfolio$CustomerID, 1000 ))
+write.csv(portfolio[CustomerID %in% midsizeCustomers, c("CustomerID", "Snapshot")], 
+          paste(data_folder,paste("santa_snapshots-", length(midsizeCustomers), ".csv", sep=""),sep="/"), row.names=F)
+cat("IH interactions for the mid-size set:",nrow(interactionsIHStyle[CustomerID %in% midsizeCustomers]),"additions only:",nrow(interactionsIHStyle[CustomerID %in% midsizeCustomers & Outcome==1]),fill=T)
+
+# Identify a small set of active customers for testing
+
+print("Test set")
+
+setkey(additions, CustomerID)
+customerSummary <- additions[, list(nAdditions=sum(.SD==1)), 
+                             by=c("CustomerID"), 
+                             .SDcols=names(additions)[grepl("ind.*", names(additions))]]
+
+activeCustomers <- sort(sample( customerSummary[ nAdditions > 1, ]$CustomerID, 10 ))
+
+cat("These customers added one or more products over time:", activeCustomers, fill=T)
+testAdditionsOverview <- additions[CustomerID %in% activeCustomers, list(nAdditions=sum(.SD==1)), 
+                                   by=c("CustomerID","Snapshot"), 
+                                   .SDcols=names(additions)[grepl("ind.*", names(additions))]] %>% spread(Snapshot, nAdditions, fill=0)
+testAdditionsOverview$`Sum5-16` <- rowSums(testAdditionsOverview[, which(names(testAdditionsOverview) %in% as.character(seq(from=5,to=16))), with=F])
+testAdditionsOverview$`Sum12-16` <- rowSums(testAdditionsOverview[, which(names(testAdditionsOverview) %in% as.character(seq(from=12,to=16))), with=F])
+testAdditionsOverview$`Sum-All` <- rowSums(testAdditionsOverview[, which(names(testAdditionsOverview) %in% as.character(seq(from=1,to=18))), with=F])
+print(testAdditionsOverview)
+
+print(interactionsIHStyle[CustomerID %in% activeCustomers & Outcome==1])
+
+write.csv(portfolio[CustomerID %in% activeCustomers, c("CustomerID", "Snapshot")], 
+          paste(data_folder,"santa_snapshots-small.csv",sep="/"), row.names=F)
+cat("Nr of test records:", nrow(portfolio[CustomerID %in% activeCustomers]), fill=T)
+cat("Nr of additions in the test set:", sum(additions[CustomerID %in% activeCustomers] == 1), fill=T)
+cat("IH interactions for the test set:",nrow(interactionsIHStyle[CustomerID %in% activeCustomers]),"additions only:",nrow(interactionsIHStyle[CustomerID %in% activeCustomers & Outcome==1]),fill=T)
+
+print("Finding subsequent additions")
+additionsTall <- interactionsIHStyle[Outcome == 1]
+additionsTallNextMonth <- copy(additionsTall)
+additionsTallNextMonth[, Snapshot := Snapshot - 1] # join with prev month
+subsequentAdditions <- merge(additionsTall, additionsTallNextMonth, by=c("CustomerID", "Snapshot"), suffixes=c(".current",".next"))
+subsequentAdditionsSummary <- merge(subsequentAdditions[, .(n = .N), by=c("Proposition.current","Proposition.next")][order(n,decreasing = T)],
+                                    additionsTall[, .(nTotalAdditions=.N, pctTotalAdditions=.N/nrow(additionsTall)), by=Proposition],
+                                    all.x = T,
+                                    by.x="Proposition.current",
+                                    by.y="Proposition")
+
+print(head(subsequentAdditionsSummary))
+
+print("Few examples of subsequent additions")
+print(head( subsequentAdditions[Proposition.current == subsequentAdditionsSummary[1]$Proposition.current & Proposition.next == subsequentAdditionsSummary[1]$Proposition.next] ))
+print(head( subsequentAdditions[Proposition.current == subsequentAdditionsSummary[2]$Proposition.current & Proposition.next == subsequentAdditionsSummary[2]$Proposition.next] ))
+
+library(igraph)
+set.seed(1234)
+gd <- head( subsequentAdditionsSummary, 75 )
+gd <- gd[Proposition.next %in% gd$Proposition.current]  # only nodes that in turn have another destination
+plot( graph.data.frame(gd), vertex.size=trunc(100*gd$pctTotalAdditions), edge.arrow.size=0.02, edge.label=gd$n, edge.label.cex=0.7, layout=layout.circle,
+      main="Most frequent subsequent additions")
+
 print("Done")
 
-# 18603/6 is a good test case : 1 addition
